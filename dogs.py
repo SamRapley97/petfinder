09f7d6d2e0c8daa6b4758fd9ff9455a65d34b218
Sam <samrapley@protonmail.com>
2024-03-17 17:43:38 +0000

    dynamic breed selection

---
+++ dogs.py
@@ -0,0 +1,35 @@
+import os
+import json
+import petpy
+from dotenv import load_dotenv
+from flask import request
+
+
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+API_SECRET = os.getenv('API_SECRET')
+
+pf = petpy.Petfinder(key=API_KEY, secret=API_SECRET)
+
+def fetch_dog_breeds():
+    all_dog_breeds = pf.breeds(types='dog')
+    return all_dog_breeds
+
+def fetch_dogs(selected_dog_breed):
+    # Specify multiple breeds in a list
+    # breeds = ["Labrador Retriever"]
+    results = []
+
+  
+    dog_search = pf.animals(animal_type="dog", breed=selected_dog_breed, location="San Antonio, TX", distance="50")
+    results.extend(dog_search["animals"])
+    
+    # Save the combined results to a JSON file
+    with open('data.json', 'w', encoding='utf-8') as f:
+        x = json.dump({"animals": results}, f, ensure_ascii=False, indent=4)    
+    
+    return x
+
+
--- main.py
+++ main.py
@@ -1,43 +1,26 @@
-import petpy
-import os
+from flask import Flask, request, render_template, jsonify
 import json
-from dotenv import load_dotenv
-from flask import Flask, render_template, jsonify
+import dogs
 
-load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
-API_SECRET = os.getenv('API_SECRET')
-
-pf = petpy.Petfinder(key=API_KEY, secret=API_SECRET)
-
-# Specify multiple breeds in a list
-breeds = ["Labrador Retriever", "Spaniel"]
-combined_results = []
-
-# Make a separate API request for each breed and combine the results
-for breed in breeds:
-    result = pf.animals(animal_type="dog", breed=breed, house_trained=True, location="San Antonio, TX", distance="50")
-    combined_results.extend(result["animals"])
-
-# Save the combined results to a JSON file
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump({"animals": combined_results}, f, ensure_ascii=False, indent=4)
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def display_json():
+
+    all_dog_breeds = dogs.fetch_dog_breeds()
+
+    dogs.fetch_dogs(request.form.get("selected_dog_breed"))
 
     # Read the data from the JSON file
     with open('data.json', 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
 
     # Pass the JSON data to the HTML template
-    return render_template('hello.html', data=data)
+    return render_template('hello.html', data=data, all_dog_breeds=all_dog_breeds)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
 
